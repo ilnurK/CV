874a3d0c0040e48e0bcc9643ced6a1fe5564d06a
Ilnurk <sky.i@bk.ru>
2023-11-26 00:04:33 +0500

    clang-formatted, add readme

---
+++ c_projects/game_of_life/README_GAME_OF_LIFE.md
@@ -0,0 +1,9 @@
+# Game of Life
+## It is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves.  
+## 2 modes: generated map from random or from file
+## Rules  
+It is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves.
+Any live cell with fewer than two live neighbours dies, as if by underpopulation.  
+Any live cell with two or three live neighbours lives on to the next generation.  
+Any live cell with more than three live neighbours dies, as if by overpopulation.  
+Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction  
--- c_projects/game_of_life/make_figures.c
+++ c_projects/game_of_life/make_figures.c
@@ -2,66 +2,62 @@
 
 #include "config.h"
 
-void Figure0(FILE* fp, int y, int x);
-void Figure1(FILE* fp, int y, int x);
-void Figure2(FILE* fp, int y, int x);
-void Figure3(FILE* fp, int y, int x);
-void Figure4(FILE* fp, int y, int x);
-void Figure5(FILE* fp, int y, int x);
-void Figure6(FILE* fp, int y, int x);
-void Figure7(FILE* fp, int y, int x);
-void Figure8(FILE* fp, int y, int x);
-void Figure9(FILE* fp, int y, int x);
+void Figure0(FILE *fp, int y, int x);
+void Figure1(FILE *fp, int y, int x);
+void Figure2(FILE *fp, int y, int x);
+void Figure3(FILE *fp, int y, int x);
+void Figure4(FILE *fp, int y, int x);
+void Figure5(FILE *fp, int y, int x);
+void Figure6(FILE *fp, int y, int x);
+void Figure7(FILE *fp, int y, int x);
+void Figure8(FILE *fp, int y, int x);
+void Figure9(FILE *fp, int y, int x);
 
 int main() {
-    char filename[6] = "0.txt";
-    void (*funcs[10])(FILE*, int, int) = {Figure0, Figure1, Figure2, Figure3,
-                                          Figure4, Figure5, Figure6, Figure7,
-                                          Figure8, Figure9};
-    for (int i = 0; i < 10; i++) {
-        filename[0] = i + '0';
-        FILE* fp = fopen(filename, "w");
-        for (int y = 0; y < Y; y++) {
-            for (int x = 0; x < X; x++) {
-                funcs[i](fp, y, x);
-            }
-            // if (y != Y - 1) {
-                // fprintf(fp, "%c", '\n');
-            // }
-
-        }
-        fclose(fp);
+  char filename[6] = "0.txt";
+  void (*funcs[10])(FILE *, int, int) = {Figure0, Figure1, Figure2, Figure3,
+                                         Figure4, Figure5, Figure6, Figure7,
+                                         Figure8, Figure9};
+  for (int i = 0; i < 10; i++) {
+    filename[0] = i + '0';
+    FILE *fp = fopen(filename, "w");
+    for (int y = 0; y < Y; y++) {
+      for (int x = 0; x < X; x++) {
+        funcs[i](fp, y, x);
+      }
     }
-    return 0;
+    fclose(fp);
+  }
+  return 0;
 }
 
-void Figure0(FILE* fp, int y, int x) {
-    int rand_val = rand() % 10;
-    char symb = (rand_val == 1) ? symbol_alive : symbol_empty;
-    fprintf(fp, "%c", symb);
+void Figure0(FILE *fp, int y, int x) {
+  int rand_val = rand() % 10;
+  char symb = (rand_val == 1) ? symbol_alive : symbol_empty;
+  fprintf(fp, "%c", symb);
 }
 
-void Figure1(FILE* fp, int y, int x) {
-    if ((y == Y / 2 || y == 3 || y == Y - 3) &&
-        (x == X / 2 - 1 || x == X / 2 || x == X / 2 + 1)) {
-        fprintf(fp, "%c", symbol_alive);
-    } else {
-        fprintf(fp, "%c", symbol_empty);
-    }
+void Figure1(FILE *fp, int y, int x) {
+  if ((y == Y / 2 || y == 3 || y == Y - 3) &&
+      (x == X / 2 - 1 || x == X / 2 || x == X / 2 + 1)) {
+    fprintf(fp, "%c", symbol_alive);
+  } else {
+    fprintf(fp, "%c", symbol_empty);
+  }
 }
 
-void Figure2(FILE* fp, int y, int x) { ; }
+void Figure2(FILE *fp, int y, int x) { ; }
 
-void Figure3(FILE* fp, int y, int x) { ; }
+void Figure3(FILE *fp, int y, int x) { ; }
 
-void Figure4(FILE* fp, int y, int x) { ; }
+void Figure4(FILE *fp, int y, int x) { ; }
 
-void Figure5(FILE* fp, int y, int x) { ; }
+void Figure5(FILE *fp, int y, int x) { ; }
 
-void Figure6(FILE* fp, int y, int x) { ; }
+void Figure6(FILE *fp, int y, int x) { ; }
 
-void Figure7(FILE* fp, int y, int x) { ; }
+void Figure7(FILE *fp, int y, int x) { ; }
 
-void Figure8(FILE* fp, int y, int x) { ; }
+void Figure8(FILE *fp, int y, int x) { ; }
 
-void Figure9(FILE* fp, int y, int x) { ; }
+void Figure9(FILE *fp, int y, int x) { ; }
