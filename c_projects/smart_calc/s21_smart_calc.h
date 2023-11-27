8ad2c1437e26044d22e8d38530624106f88020c4
Ilnurk <sky.i@bk.ru>
2023-11-27 20:26:11 +0500

    delete dir smart_calc

--- c_projects/smart_calc/s21_smart_calc.h
+++
@@ -1,6 +0,0 @@
-#ifndef SMART_CALC_H
-#define SMART_CALC_H
-
-#include "calc_gui/s21_main_window.h"
-
-#endif
--- c_projects/smart_calc/tests/tests.h
+++
@@ -1,6 +0,0 @@
-#ifndef TEST_H
-#define TEST_H
-
-void tests_credit(int *nf);
-void tests_deposit(int *nf);
-#endif
