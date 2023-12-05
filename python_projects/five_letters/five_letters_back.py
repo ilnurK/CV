d98ba2d526fface53f4ba2438f5cc7c67480810b
Ilnurk <sky.i@bk.ru>
2023-12-05 17:17:36 +0500

    logic upd, error with true_fit fix

--- python_projects/five_letters/five_letters_back.py
+++ python_projects/five_letters/five_letters_back.py
@@ -1,59 +1,29 @@
 FILE_NAME = "dict.txt"
 ENCODE = "CP1251"
-BLANK = '\0'
+ENTRY_FIT = "entry_fit"
+ENTRY_OUT = "entry_out"
+ENTRY_NOT = "entry_not"
+BUT_SEARCH = "but_search"
+FIT_LIST = "fit_words"
+BLANK = "\0"
 N = 5
 
-def on_entry_fitted_changed(entry, true_fit):
-    text = entry.get_text()
-    name = entry.get_name()
-    if text:
-        if name == "e_letter1":
-            true_fit[0] = text[0]
-        elif name == "e_letter2":
-            true_fit[1] = text[0]
-        elif name == "e_letter3":
-            true_fit[2] = text[0]
-        elif name == "e_letter4":
-            true_fit[3] = text[0]
-        elif name == "e_letter5":
-            true_fit[4] = text[0]
-
-def on_entry_out_of_pos_changed(entry, out_of_pos_fit):
-    text = entry.get_text()
-    name = entry.get_name()
-    if text:
-        if name == "e_letter_pos1":
-            out_of_pos_fit[0] = text[0]
-        elif name == "e_letter_pos2":
-            out_of_pos_fit[1] = text[0]
-        elif name == "e_letter_pos3":
-            out_of_pos_fit[2] = text[0]
-        elif name == "e_letter_pos4":
-            out_of_pos_fit[3] = text[0]
-        elif name == "e_letter_pos5":
-            out_of_pos_fit[4] = text[0]
-
-def on_entry_no_fit_changed(entry, not_fit):
-    not_fit[0] = entry.get_text()   
 
 def check_fit_letters(word, true_fit, flag_fit):
     for i in range(N):
         c = true_fit[i].upper()
-        if c == BLANK or c == word[i]:
-            continue
-        else:
+        if c != BLANK and c != word[i]:
             flag_fit = 0
     return flag_fit
+
 
 def check_pos_letters(word, out_of_pos_fit, flag_fit):
     for i in range(N):
         c = out_of_pos_fit[i].upper()
-        if c != BLANK:
-            if c in word and word.find(c) != i:
-                continue
-            else:
-                flag_fit = 0
+        if c != BLANK and c not in word or word.find(c) == i:
+            flag_fit = 0
     return flag_fit
+
 
 def check_no_letters(word, not_fit, flag_fit):
     for i in range(len(not_fit[0])):
@@ -61,6 +31,7 @@ def check_no_letters(word, not_fit, flag_fit):
         if c in word:
             flag_fit = 0
     return flag_fit
+
 
 def check_word(word, true_fit, out_of_pos_fit, not_fit, fit_words):
     flag_fit = 1
@@ -75,31 +46,58 @@ def check_word(word, true_fit, out_of_pos_fit, not_fit, fit_words):
         flag_find = 1
     return flag_find
 
+
 def search_cycle(words, data):
     flag_find = 0
-    true_fit = data["true"]
-    out_of_pos_fit = data["out"]
-    not_fit = data["not"]
-    fit_words = data["list"]
+    true_fit = data[ENTRY_FIT]
+    out_of_pos = data[ENTRY_OUT]
+    not_fit = data[ENTRY_NOT]
+    fit_words = data[FIT_LIST]
     for word in words:
-        if len(word) == 6: 
-           word = word.upper() 
-           if check_word(word, true_fit, out_of_pos_fit, not_fit, fit_words):
-               flag_find = 1
+        if len(word) == 6:
+            word = word.upper()
+            if check_word(word, true_fit, out_of_pos, not_fit, fit_words):
+                flag_find = 1
     return flag_find
-    
 
-def on_search_clicked(button, result, data):
-    data["list"].clear()
+
+def get_data(builder):
+    true_fit, out_of_pos, not_fit, fit_words = [], [], [], []
+    data = {
+        ENTRY_FIT: true_fit,
+        ENTRY_OUT: out_of_pos,
+        ENTRY_NOT: not_fit,
+        FIT_LIST: fit_words,
+    }
+    entries = []
+    for i in range(N):
+        entries.append(builder.get_object(ENTRY_FIT + str(i)))
+    for e in entries:
+        text = e.get_text()
+        text = text if (len(text)) else BLANK
+        data[ENTRY_FIT].append(text)
+    entries.clear()
+    for i in range(N):
+        entries.append(builder.get_object(ENTRY_OUT + str(i)))
+    for e in entries:
+        text = e.get_text()
+        text = text if (len(text)) else BLANK
+        data[ENTRY_OUT].append(text)
+    data[ENTRY_NOT].append(builder.get_object(ENTRY_NOT).get_text())
+    return data
+
+
+def on_search_clicked(button, result, builder):
+    data = get_data(builder)
     flag_find = 0
     name = button.get_name()
-    if name == "but_search":
+    if name == BUT_SEARCH:
         with open(FILE_NAME, "r", encoding=ENCODE) as inp:
             words = inp.readlines()
-            flag_find = search_cycle(words, data)        
+            flag_find = search_cycle(words, data)
     text = ""
-    fitted_words = data["list"]
-    if flag_find and len(fitted_words) < 100 :
+    fitted_words = data[FIT_LIST]
+    if flag_find and len(fitted_words) < 100:
         text = "".join(fitted_words)
     elif flag_find:
         text = "".join([fitted_words[i] for i in range(100)])
@@ -107,4 +105,3 @@ def on_search_clicked(button, result, data):
     text_buffer = result.get_buffer()
     text_buffer.set_text("")
     text_buffer.set_text(text)
-    
--- python_projects/five_letters/five_letters_gui.py
+++ python_projects/five_letters/five_letters_gui.py
@@ -1,55 +1,22 @@
 import gi
-gi.require_version('Gtk', '3.0')
+
+gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 import five_letters_back as fb
+
 
 def init_widgets(builder):
     window = builder.get_object("window")
     window.connect("destroy", Gtk.main_quit)
-        
-    letter1 = builder.get_object("e_letter1")
-    letter2 = builder.get_object("e_letter2")
-    letter3 = builder.get_object("e_letter3")
-    letter4 = builder.get_object("e_letter4")
-    letter5 = builder.get_object("e_letter5")
-    
-    true_fit = [fb.BLANK for i in range(fb.N)]
-    
-    letter1.connect("changed", lambda ent: fb.on_entry_fitted_changed(ent, true_fit))
-    letter2.connect("changed", lambda ent: fb.on_entry_fitted_changed(ent, true_fit))
-    letter3.connect("changed", lambda ent: fb.on_entry_fitted_changed(ent, true_fit))
-    letter4.connect("changed", lambda ent: fb.on_entry_fitted_changed(ent, true_fit))
-    letter5.connect("changed", lambda ent: fb.on_entry_fitted_changed(ent, true_fit))
-
-    letter_pos1 = builder.get_object("e_letter_pos1")
-    letter_pos2 = builder.get_object("e_letter_pos2")
-    letter_pos3 = builder.get_object("e_letter_pos3")
-    letter_pos4 = builder.get_object("e_letter_pos4")
-    letter_pos5 = builder.get_object("e_letter_pos5")
-    
-    out_of_pos_fit = [fb.BLANK for i in range(fb.N)]
-        
-    letter_pos1.connect("changed", lambda ent: fb.on_entry_out_of_pos_changed(ent,  out_of_pos_fit))
-    letter_pos2.connect("changed", lambda ent: fb.on_entry_out_of_pos_changed(ent,  out_of_pos_fit))
-    letter_pos3.connect("changed", lambda ent: fb.on_entry_out_of_pos_changed(ent,  out_of_pos_fit))
-    letter_pos4.connect("changed", lambda ent: fb.on_entry_out_of_pos_changed(ent,  out_of_pos_fit))
-    letter_pos5.connect("changed", lambda ent: fb.on_entry_out_of_pos_changed(ent,  out_of_pos_fit))
-
-    letters_no_fit = builder.get_object("e_no_fit")
-    
-    not_fit = list()
-    not_fit.append("")
-
-    letters_no_fit.connect("changed", lambda ent: fb.on_entry_no_fit_changed(ent, not_fit))
 
     but_search = builder.get_object("but_search")
     result = builder.get_object("result")
-        
-    fit_words = list()
-    data = {"true": true_fit, "out": out_of_pos_fit, "not": not_fit, "list" :fit_words}
-        
-    but_search.connect("clicked", lambda btn: fb.on_search_clicked(btn, result, data))
+
+    but_search.connect(
+        "clicked", lambda btn: fb.on_search_clicked(btn, result, builder)
+    )
     return window
+
 
 def five_letters_gui(glade_name):
     builder = Gtk.Builder()
@@ -60,6 +27,7 @@ def five_letters_gui(glade_name):
         Gtk.main()
     except BaseException:
         print("Ошибка открытия файла")
+
 
 glade_name = "five_letters_gui.glade"
 five_letters_gui(glade_name)
